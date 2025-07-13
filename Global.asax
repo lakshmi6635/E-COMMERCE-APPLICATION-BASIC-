f09081be53fd34dcd475b2391539f14cda54b6c4
Lakshmi M <lakshmi280705@gmail.com>
2025-07-13 18:59:01 +0530

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasicECommerceApp.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using BasicECommerceApp.Models;
+
+namespace BasicECommerceApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // Seed data
+            using (var context = new ECommerceDBContext())
+            {
+                if (!context.Products.Any())
+                {
+                    context.Products.Add(new Product { Name = "Laptop", Price = 50000 });
+                    context.Products.Add(new Product { Name = "Smartphone", Price = 15000 });
+                    context.Products.Add(new Product { Name = "Headphones", Price = 2000 });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
